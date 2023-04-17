12203f387ed3eb16fbdd1502dbff54b62704271c
LUIS980918\luis9 <luis.rendon@bpt.com.co>
2023-04-16 21:03:31 -0500

    Se suben los cambios de responsive

---
+++ components/HeaderDesktop.tsx
@@ -0,0 +1,35 @@
+const HeaderDesktop = () => {
+    return (
+        <div className="flex-header-p1">
+            <div className="gap-items-header">
+                <img src="resources/icons/mercado-libre.svg" alt="Mercado libre"/>
+                <ul>
+                    <li>Ingresa tu dominio</li>
+                </ul>
+            </div>
+            <img className="gap-items-header-mobile" src="resources/icons/logo_small.png" alt="Mercado libre"/>
+            <div className="gap-items-header-input">
+                <div className="input-header-height">
+                    <form className="form-f">
+                        <input className="input-header" placeholder="Buscar productos, marcas y más…"/>
+                    </form>
+                </div>
+                <ul className="categories">
+                    <div className="icon-category">
+                        <li>Categorías</li>
+                    </div>
+                    <li>Ofertas</li>
+                    <li>Historial</li>
+                    <li>Supermercado</li>
+                    <li>Moda</li>
+                    <li>Vender</li>
+                    <li>Ayuda / PQR</li>
+                </ul>
+            </div>
+            <img className="nav-bar-mobile" src="resources/icons/nav_ico.png"/>
+            <img className="icon-car-mobile" src="resources/icons/cart-shopping-solid 1.svg"/>
+        </div>
+    )
+}
+
+export {HeaderDesktop}
