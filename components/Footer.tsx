2a1b9274638e619a0752549b134f3c04d9f613d4
LUIS980918\luis9 <luis.rendon@bpt.com.co>
2023-04-16 21:13:32 -0500

    Se suben algunos ajustes faltantes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,44 +5,44 @@ const Footer = () => {
     <div>
         <div className="card-8-all">
             <div className="card-8">
-                <img src="resources/icons/payment.svg" alt="Payment" />
-                <div className="footer-info-title-1">Paga con tarjeta o en efectivo</div>
-                <div className="footer-info-title-2">Con Mercado Pago, paga en cuotas y aprovecha la comodidad de
+                <img className="default-image-configuration-footer" src="resources/icons/payment.svg" alt="Payment" />
+                <div className="footer-info-title-1 default-font-style">Paga con tarjeta o en efectivo</div>
+                <div className="footer-info-title-2 default-font-style">Con Mercado Pago, paga en cuotas y aprovecha la comodidad de
                     financiación que te da tu banco, o
                     hazlo con efectivo en puntos de pago. ¡Y siempre es seguro!</div>
-                <a href="#" className="card-8-more-info">Cómo pagar con Mercado Pago</a>
+                <a href="#" className="card-8-more-info default-font-style">Cómo pagar con Mercado Pago</a>
             </div>
             <div className="card-8-af-be">
-                <img src="resources/icons/shipping.svg" alt="Shipping" />
-                <div className="footer-info-title-1">Envío gratis desde $ 90.000</div>
-                <div className="footer-info-title-2">Con solo estar registrado en Mercado Libre, tienes envíos gratis en
+                <img className="default-image-configuration-footer" src="resources/icons/shipping.svg" alt="Shipping" />
+                <div className="footer-info-title-1 default-font-style">Envío gratis desde $ 90.000</div>
+                <div className="footer-info-title-2 default-font-style">Con solo estar registrado en Mercado Libre, tienes envíos gratis en
                     miles de productos
                     seleccionados.</div>
-                <a href="#" className="card-8-more-info">Conoce más sobre este beneficio</a>
+                <a href="#" className="card-8-more-info default-font-style">Conoce más sobre este beneficio</a>
             </div>
             <div className="card-8">
-                <img src="resources/icons/protected.svg" alt="Protected" />
-                <div className="footer-info-title-1">Seguridad, de principio a fin</div>
-                <div className="footer-info-title-2">¿No te gusta? ¡Devuélvelo! En Mercado Libre, no hay nada que no
+                <img className="default-image-configuration-footer" src="resources/icons/protected.svg" alt="Protected" />
+                <div className="footer-info-title-1 default-font-style">Seguridad, de principio a fin</div>
+                <div className="footer-info-title-2 default-font-style">¿No te gusta? ¡Devuélvelo! En Mercado Libre, no hay nada que no
                     puedas hacer, porque estás
                     siempre protegido.</div>
-                <a href="#" className="card-8-more-info">Cómo te protegemos</a>
+                <a href="#" className="card-8-more-info default-font-style">Cómo te protegemos</a>
             </div>
         </div>
         <div className="part-2-footer">
             <ul className="ul-footer">
-                <li>Trabaja con nosotros</li>
-                <li>Términos y condiciones</li>
-                <li>Cómo cuidamos tu privacidad</li>
-                <li>Accesibilidad</li>
-                <li>Ayuda / PQR</li>
-                <li>www.sic.gov.co</li>
+                <li className="default-font-style">Trabaja con nosotros</li>
+                <li className="default-font-style">Términos y condiciones</li>
+                <li className="default-font-style">Cómo cuidamos tu privacidad</li>
+                <li className="default-font-style">Accesibilidad</li>
+                <li className="default-font-style">Ayuda / PQR</li>
+                <li className="default-font-style">www.sic.gov.co</li>
             </ul>
-            <div className="texto-info-footer">Copyright © 1999-2023 MercadoLibre Colombia LTDA.</div>
-            <div className="texto-info-footer">Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</div>
+            <div className="texto-info-footer default-font-style">Copyright © 1999-2023 MercadoLibre Colombia LTDA.</div>
+            <div className="texto-info-footer default-font-style">Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</div>
             <div className="waring-footer">
-                <img src="resources/icons/industria_comercio.svg" alt="Industria y comercio" />
-                <img src="resources/icons/pare_compre.svg" alt="Pare y compre" />
+                <img className="default-image-configuration-footer" src="resources/icons/industria_comercio.svg" alt="Industria y comercio" />
+                <img className="default-image-configuration-footer" src="resources/icons/pare_compre.svg" alt="Pare y compre" />
             </div>
         </div>
     </div>
